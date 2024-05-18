8cf2ad8160a97a4c07ce4a76343e2c584be9a30f
jiwonpg98 <jiwonpg98@gmail.com>
2024-05-18 22:46:17 +0900

    test: jwt 발급과 date util 테스트 코드 추가

---
+++ src/test/kotlin/kr/co/shoppingcart/cart/auth/JwtProviderTest.kt
@@ -0,0 +1,77 @@
+package kr.co.shoppingcart.cart.auth
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.io.Decoders
+import io.jsonwebtoken.security.Keys
+import kr.co.shoppingcart.cart.utils.DateUtil
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.ZonedDateTime
+import javax.crypto.SecretKey
+
+@SpringBootTest
+@DisplayName("jwt 생성 및 검증 테스트")
+class JwtProviderTest (
+    @Value("\${jwt.secret}")
+    private val secret: String,
+    @Value("\${jwt.issuer}")
+    private val issuer: String,
+    @Autowired
+    private var jwtProvider: JwtProvider
+) {
+    @Test
+    fun contextLoads() {
+        assertNotNull(secret)
+        assertNotNull(issuer)
+    }
+
+    @BeforeEach
+    fun setUp() {
+        jwtProvider = JwtProvider(secret, issuer)
+    }
+
+    @Test
+    @DisplayName("jwt 생성 테스트")
+    fun `jwt 생성에 성공`() {
+        // given
+        val secretKey: SecretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret))
+        val jwtPayload = createJwtPayload(
+            email = email,
+            expiredTimestamp = 86400,
+            issuedAt = ZonedDateTime.now()
+        )
+        val expiration = jwtPayload.now.plusSeconds(jwtPayload.expiredTimestamp)
+
+        // when
+        val jwt: String = jwtProvider.createJwt(jwtPayload)
+
+        // then
+        val claimsJwt = Jwts.parser().verifyWith(secretKey).build()
+            .parseSignedClaims(jwt)
+
+        assertEquals(claimsJwt.payload.issuer, issuer)
+        assertEquals(claimsJwt.payload["email"], email)
+        assertEquals(
+            claimsJwt.payload.expiration.toString(),
+            DateUtil.convertZoneDateTimeToDate(expiration).toString()
+        )
+    }
+
+    private fun createJwtPayload(email: String, expiredTimestamp: Long, issuedAt: ZonedDateTime): JwtPayload
+        = JwtPayload(
+            email = email,
+            expiredTimestamp = expiredTimestamp,
+            now = issuedAt
+        )
+
+    companion object {
+        private val email = "test@test.com"
+    }
+
+}
---
+++ src/test/kotlin/kr/co/shoppingcart/cart/utils/DateUtilTest.kt
@@ -0,0 +1,25 @@
+package kr.co.shoppingcart.cart.utils
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import java.text.SimpleDateFormat
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
+
+@DisplayName("Date Util 테스트")
+class DateUtilTest {
+    @DisplayName("ZoneDateTime 시간 -> Date 형식으로 변경")
+    @Test
+    fun convertZoneDateTimeToDateTest() {
+        val testDate: Date = DateUtil.convertZoneDateTimeToDate(zoneDateTime)
+        assertEquals(testDate, dateTime)
+    }
+
+    companion object {
+        private val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss")
+        private val zoneDateTime = ZonedDateTime.parse("2024.05.18 16:00:00", dateTimeFormatter.withZone(java.time.ZoneId.systemDefault()))
+        private val dateTime = SimpleDateFormat("yyyy.MM.dd HH:mm:ss").parse("2024.05.18 16:00:00")
+    }
+}
