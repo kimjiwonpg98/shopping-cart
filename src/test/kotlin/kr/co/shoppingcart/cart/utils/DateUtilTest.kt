0af18b295944a4b90bfc3ba2bf022e0af750cdfa
jiwonpg98 <jiwonpg98@gmail.com>
2024-07-14 16:54:57 +0900

    test: util test 추가

--- src/test/kotlin/kr/co/shoppingcart/cart/utils/DateUtilTest.kt
+++ src/test/kotlin/kr/co/shoppingcart/cart/utils/DateUtilTest.kt
@@ -17,6 +17,13 @@ class DateUtilTest {
         assertEquals(testDate, dateTime)
     }
 
+    @DisplayName("ZonedDateTime -> 서울 리전으로 포맷팅")
+    @Test
+    fun convertZonedDateTimeToSeoulTimeTest() {
+        val testDate: ZonedDateTime = DateUtil.convertZonedDateTimeToSeoulTime(utcDateTime)
+        assertEquals(testDate, zoneDateTime)
+    }
+
     companion object {
         private val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss")
         private val zoneDateTime =
@@ -25,5 +32,10 @@ class DateUtilTest {
                 dateTimeFormatter.withZone(java.time.ZoneId.systemDefault()),
             )
         private val dateTime = SimpleDateFormat("yyyy.MM.dd HH:mm:ss").parse("2024.05.18 16:00:00")
+        private val utcDateTime =
+            ZonedDateTime.parse(
+                "2024.05.18 07:00:00",
+                dateTimeFormatter.withZone(java.time.ZoneId.of("UTC")),
+            )
     }
 }
