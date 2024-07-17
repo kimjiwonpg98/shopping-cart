2b1f0b89e46caf29aadba7b8bef48041e170f6bd
jiwonpg98 <jiwonpg98@gmail.com>
2024-07-17 23:26:45 +0900

    feat: 에러메시지 추가

--- src/main/kotlin/kr/co/shoppingcart/cart/common/error/model/ExceptionCode.kt
+++ src/main/kotlin/kr/co/shoppingcart/cart/common/error/model/ExceptionCode.kt
@@ -11,6 +11,10 @@ enum class ExceptionCode(
     // 토큰 검증 후 오류
     E_401_000(401_000, HttpStatus.UNAUTHORIZED),
     E_403_000(403_000, HttpStatus.FORBIDDEN),
+    E_403_001(403_001, HttpStatus.FORBIDDEN),
+
+    E_404_000(404_000, HttpStatus.NOT_FOUND),
+    E_404_001(404_001, HttpStatus.NOT_FOUND),
 
     E_500_000(500_000, HttpStatus.INTERNAL_SERVER_ERROR),
 }
