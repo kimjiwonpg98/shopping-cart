3cd8ebbce430b51d420258009ddc92d670a41d24
jiwonpg98 <jiwonpg98@gmail.com>
2024-08-18 15:43:37 +0900

    test: cache 테스트부 추가

---
+++ src/test/kotlin/kr/co/shoppingcart/cart/usecase/BasketUseCaseCacheTest.kt
@@ -0,0 +1,59 @@
+package kr.co.shoppingcart.cart.usecase
+
+import kr.co.shoppingcart.cart.config.cache.CacheType
+import kr.co.shoppingcart.cart.domain.basket.BasketUseCase
+import kr.co.shoppingcart.cart.domain.basket.vo.Basket
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cache.Cache
+import org.springframework.cache.CacheManager
+import org.springframework.test.context.TestConstructor
+
+@SpringBootTest
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+class BasketUseCaseCacheTest(
+    val basketUseCase: BasketUseCase,
+    val cacheManager: CacheManager,
+) {
+    @BeforeEach
+    fun setUp() {
+        cacheManager.getCache(CacheType.TEMPLATE_CACHE.cacheName)?.clear()
+    }
+
+    @Test
+    fun `캐시가 존재하면 캐시를 조회`() {
+        val first = basketUseCase.getByTemplateIdAndSizeOrderByUpdatedDesc(templateId = testTemplateId, size = size)
+        val second = basketUseCase.getByTemplateIdAndSizeOrderByUpdatedDesc(templateId = testTemplateId, size = size)
+        val cache = getCacheTemplateId(testTemplateId)
+        assertEquals(second, cache)
+    }
+
+    @Test
+    fun `캐시가 존재하지 않으면 캐시해서 가져오지 않는다`() {
+        basketUseCase.getByTemplateIdAndSizeOrderByUpdatedDesc(templateId = testTemplateId, size = size)
+        val cache = getCacheTemplateId(testTemplateId)
+        assertNull(cache)
+    }
+
+    private fun getCacheTemplateId(templateId: Long): List<Basket> =
+        cacheManager.getCache(CacheType.TEMPLATE_CACHE.cacheName)?.get(templateId).let { cachedValue ->
+            if (cachedValue is Cache.ValueWrapper) {
+                val value = cachedValue.get()
+                if (value is List<*>) {
+                    value.filterIsInstance<Basket>()
+                } else {
+                    emptyList()
+                }
+            } else {
+                emptyList()
+            }
+        }
+
+    companion object {
+        val testTemplateId: Long = 1
+        val size = 3
+    }
+}
