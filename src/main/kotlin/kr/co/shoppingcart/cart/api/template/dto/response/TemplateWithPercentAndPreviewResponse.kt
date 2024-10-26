e0d5d24d0619dcf153283ffc72e40b8221e056e8
jiwonpg98 <jiwonpg98@gmail.com>
2024-10-26 17:55:50 +0900

    refactor: 응답 추가

---
+++ src/main/kotlin/kr/co/shoppingcart/cart/api/template/dto/response/TemplateWithPercentAndPreviewResponse.kt
@@ -0,0 +1,15 @@
+package kr.co.shoppingcart.cart.api.template.dto.response
+
+import java.time.ZonedDateTime
+
+data class TemplateWithPercentAndPreviewResponse(
+    val id: Long,
+    val name: String,
+    val userId: Long,
+    val percent: Int,
+    val isPublic: Boolean,
+    val thumbnailIndex: Int,
+    val createTime: ZonedDateTime,
+    val updateTime: ZonedDateTime,
+    val preview: List<String>,
+)
