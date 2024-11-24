a7de37be45ba7589f3dd00a38ecbea991328fd4a
jiwonpg98 <jiwonpg98@gmail.com>
2024-11-24 18:13:50 +0900

    chore: 변경점 추가

--- src/main/kotlin/kr/co/shoppingcart/cart/auth/CustomSuccessHandler.kt
+++ src/main/kotlin/kr/co/shoppingcart/cart/auth/CustomSuccessHandler.kt
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseCookie
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.security.web.DefaultRedirectStrategy
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
 import org.springframework.stereotype.Service
 
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Service
 class CustomSuccessHandler(
     private val tokenService: TokenService,
 ) : AuthenticationSuccessHandler {
+    private val redirectStrategy = DefaultRedirectStrategy()
+
     override fun onAuthenticationSuccess(
         request: HttpServletRequest,
         response: HttpServletResponse,
@@ -39,22 +42,24 @@ class CustomSuccessHandler(
             ResponseCookie
                 .from("accessToken", accessToken)
                 .httpOnly(true)
-//                .secure(true)
-                .sameSite("None")
+                .secure(true)
+                .path("/")
+                .sameSite("Strict")
                 .build()
                 .toString(),
         )
+
         response.addHeader(
             HttpHeaders.SET_COOKIE,
             ResponseCookie
                 .from("refreshToken", refreshToken)
                 .httpOnly(true)
-//                .secure(true)
-                .sameSite("None")
+                .secure(true)
+                .path("/")
                 .build()
                 .toString(),
         )
 
-        response.sendRedirect("http://localhost:3000/home")
+        redirectStrategy.sendRedirect(request, response, "https://kka-dam.vercel.app/home")
     }
 }
